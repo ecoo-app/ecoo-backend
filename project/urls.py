2aab4179fe6757df5741dc71b6e5576195e082d8
Lionel Perrin <l.perrin@papers.ch>
2020-07-07 15:06:14 +0200

    feat(txs): add signature verification of transaction

--- project/urls.py
+++ project/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     path('api/wallet/', include('apps.wallet.urls')),
     path('api/auth/', include('apps.custom_auth.urls')),
     path('auth/', include('rest_framework_social_oauth2.urls')),
-    path('social/', include('social_django.urls', namespace='social')),
+    # path('social/', include('social_django.urls', namespace='social')),
 ]
 
 if settings.DEBUG:  # new
