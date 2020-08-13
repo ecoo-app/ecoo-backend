2e925fd547d7f5d57d05f6fea9b416042fb64d1e
Lukas Schönbächler <l.schoenbaechler@papers.ch>
2020-08-14 00:40:19 +0200

    feat(actions): verify user and verify company actions added

--- apps/profiles/admin.py
+++ apps/profiles/admin.py
@@ -7,7 +7,51 @@ from django import forms
 import datetime
 from django.shortcuts import render
 from apps.profiles.filters import VerificationLevelFilter
+from apps.verification.models import UserVerification, CompanyVerification, VERIFICATION_STATES
+from apps.wallet.models import Wallet
 
+def verify_users(modeladmin, request, queryset):
+
+    modified = 0
+    for user_profile in queryset.exclude(user_verification__isnull=False).exclude(sms_pin_verification__isnull=False):
+        UserVerification.objects.create(
+            user_profile=user_profile,
+            state=VERIFICATION_STATES.CLAIMED.value,
+            first_name=user_profile.first_name,
+            last_name=user_profile.last_name,
+            address_street=user_profile.address_street,
+            address_town=user_profile.address_town,
+            address_postal_code=user_profile.address_postal_code,
+            date_of_birth=user_profile.date_of_birth,
+        )
+        modified += 1
+
+    if modified > 0:
+        messages.add_message(request, messages.SUCCESS, _('{} User Profiles verified').format(modified))
+    else:
+        messages.add_message(request, messages.WARNING, _('All User Profiles were already verified'))
+
+verify_users.short_description = _('Verify users')
+
+
+def verify_companies(modeladmin, request, queryset):
+
+    modified = 0
+    for company_profile in queryset.exclude(company_verification__isnull=False): #.exclude(sms_pin_verification__isnull=False):
+        CompanyVerification.objects.create(
+            company_profile=company_profile,
+            state=VERIFICATION_STATES.CLAIMED.value,
+            name=company_profile.name,
+            uid=company_profile.uid,
+        )
+        modified += 1
+
+    if modified > 0:
+        messages.add_message(request, messages.SUCCESS, _('{} Company Profiles verified').format(modified))
+    else:
+        messages.add_message(request, messages.WARNING, _('All Company Profiles were already verified'))
+
+verify_companies.short_description = _('Verify companies')
 
 @admin.register(UserProfile)
 class UserProfile(admin.ModelAdmin):
@@ -16,7 +60,11 @@ class UserProfile(admin.ModelAdmin):
     search_fields = ['first_name', 'last_name',
                      'address_street', 'telephone_number', 'date_of_birth']
     list_filter = [VerificationLevelFilter]
+    actions = [verify_users]
 
+    def render_change_form(self, request, context, *args, **kwargs):
+        context['adminform'].form.fields['wallet'].queryset = Wallet.objects.filter(category=0)
+        return super(UserProfile, self).render_change_form(request, context, *args, **kwargs)
 
 @admin.register(CompanyProfile)
 class CompanyProfile(admin.ModelAdmin):
@@ -25,3 +73,8 @@ class CompanyProfile(admin.ModelAdmin):
     search_fields = ['name', 'uid',
                      'address_street']
     list_filter = [VerificationLevelFilter]
+    actions = [verify_companies]
+
+    def render_change_form(self, request, context, *args, **kwargs):
+        context['adminform'].form.fields['wallet'].queryset = Wallet.objects.filter(category=1)
+        return super(CompanyProfile, self).render_change_form(request, context, *args, **kwargs)
