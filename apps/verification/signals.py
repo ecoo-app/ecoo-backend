6e3c0afc9de4ca7c08470d94ddf1807a259d6b53
Alessandro De Carli <a.decarli@papers.ch>
2020-09-09 11:39:17 +0200

    feat(): 6digit pin

--- apps/verification/signals.py
+++ apps/verification/signals.py
@@ -2,27 +2,30 @@ from django.db.models import Max
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from django.conf import settings
-from secrets import token_hex
 from apps.verification.models import UserVerification, CompanyVerification, AddressPinVerification, SMSPinVerification, VERIFICATION_STATES
 from apps.verification.utils import send_sms, send_postcard
+import random
+import string
+
+system_random = random.SystemRandom()
 
 
 @receiver(pre_save, sender=SMSPinVerification, dispatch_uid='custom_sms_pin_verification_validation')
 def custom_sms_pin_verification_validation(sender, instance, **kwargs):
     if not instance.pin:
-        instance.pin = token_hex(4)
+        instance.pin = ''.join(system_random.choice(
+            string.digits) for x in range(6))
         send_sms(
             to_number=instance.user_profile.telephone_number,
-            message="""{}
-            {}""".format(instance.pin, settings.SMS_TEXT)
+            message="{} {}".format(instance.pin, settings.SMS_TEXT)
         )
 
 
 @receiver(pre_save, sender=AddressPinVerification, dispatch_uid='custom_address_pin_verification_validation')
 def custom_address_pin_verification_validation(sender, instance, **kwargs):
     if not instance.pin:
-        instance.pin = token_hex(4)
-        print(settings.POST_CARD_TEXT.format(instance.pin))
+        instance.pin = ''.join(system_random.choice(
+            string.digits) for x in range(6))
         instance.external_id = send_postcard(
             message=settings.POST_CARD_TEXT.format(instance.pin),
             company=instance.company_profile.name,
