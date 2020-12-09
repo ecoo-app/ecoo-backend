7df804283cdee10aa238f668cdb8c68a85d9d621
Alessandro De Carli <a.decarli@papers.ch>
2020-12-09 13:50:16 +0100

    fix(sync): adds sync checker

--- apps/wallet/management/commands/sync_with_blockchain.py
+++ apps/wallet/management/commands/sync_with_blockchain.py
@@ -2,7 +2,7 @@ import datetime
 
 from django.core.management.base import BaseCommand, CommandError
 
-from apps.wallet.utils import sync_to_blockchain
+from apps.wallet.utils import sync_to_blockchain, check_sync_state
 
 
 class Command(BaseCommand):
@@ -10,3 +10,4 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         sync_to_blockchain(is_dry_run=False)
+        check_sync_state()
