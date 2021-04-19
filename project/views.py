467d0fc9e1473bd768aceabc2d659b69f4700013
Alessandro De Carli <ale@Alessandros-Air.fritz.box>
2021-04-19 20:45:32 +0200

    chore(): lint

--- project/views.py
+++ project/views.py
@@ -1,10 +1,10 @@
+from urllib.parse import urlencode
+
 from django.conf import settings
 from django.shortcuts import redirect
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from urllib.parse import urlencode
-
 
 
 @api_view()
@@ -12,14 +12,20 @@ from urllib.parse import urlencode
 def version(request):
     return Response({"version": 2})
 
+
 @api_view()
 @permission_classes([AllowAny])
 def deeplink(request):
-    hotfix_link = "https://ecoo.page.link/?" + urlencode(
-                {
-                    "link": "{}wallet/?{}".format(
-                        settings.DEEPLINK_BASE_URL, request.GET.urlencode()
-                    )
-                }
-            ) + "&apn=ch.ecoupon.mobile.android&ibi=ch.ecoupon.mobile&isi=" + settings.DEEPLINK_ISI_PARAM
+    hotfix_link = (
+        "https://ecoo.page.link/?"
+        + urlencode(
+            {
+                "link": "{}wallet/?{}".format(
+                    settings.DEEPLINK_BASE_URL, request.GET.urlencode()
+                )
+            }
+        )
+        + "&apn=ch.ecoupon.mobile.android&ibi=ch.ecoupon.mobile&isi="
+        + settings.DEEPLINK_ISI_PARAM
+    )
     return redirect(hotfix_link)
