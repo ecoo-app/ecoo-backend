59be9dd854db4516236ca69a946f0ad3f4852003
Alessandro De Carli <decarli.ale@gmail.com>
2021-04-19 20:02:23 +0200

    Update views.py

--- project/views.py
+++ project/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from urllib.parse import urlencode
 
 
 @api_view()
