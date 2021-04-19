7edb8b83f8a9688d6c35cd4bfb4adfad941e4236
Alessandro De Carli <decarli.ale@gmail.com>
2021-04-19 17:58:22 +0200

    feat(redirect): redirects deeplink and appends the isi

--- project/views.py
+++ project/views.py
@@ -1,9 +1,25 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-
+from django.conf import settings
+from django.shortcuts import redirect
 
 @api_view()
 @permission_classes([AllowAny])
 def version(request):
     return Response({"version": 2})
+
+@api_view()
+@permission_classes([AllowAny])
+def deeplink(request):
+    hotfix_link = "https://ecoo.page.link/?"
+            + urlencode(
+                {
+                    "link": "{}wallet/?{}".format(
+                        settings.DEEPLINK_BASE_URL, request.GET.urlencode()
+                    )
+                }
+            )
+            + "&apn=ch.ecoupon.mobile.android&ibi=ch.ecoupon.mobile&isi="
+            + settings.DEEPLINK_ISI_PARAM
+    return redirect(hotfix_link)
